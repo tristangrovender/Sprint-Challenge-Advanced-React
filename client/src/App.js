1a0ee2b16dc0c233e05644b281cefe208fbfaf4a
Tristan Grovender <therealtristangrovender@gmail.com>
2020-05-03 15:53:29 -0500

    App mapping over data from server, displaying said data on screen. Dark mode has been activated as well.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./NavBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            footballers: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:5000/api/players")
+            .then(res => res.json())
+            .then(players => {
+                console.log(players);
+                this.setState({ footballers: players });
+            })
+            .catch(err => console.log("Error fetching data"));
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Navbar />
+                <br />
+                <ul>
+                    {this.state.footballers.map(player => {
+                        return (
+                            <li className="player-card" key={player.id}>
+                                <h2>Footballer Name: {player.name}</h2>
+                                <p>Represents country: {player.country}</p>
+                                <p># of times searched: {player.searches}</p>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default App;
