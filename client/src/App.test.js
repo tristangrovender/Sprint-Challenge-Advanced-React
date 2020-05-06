dc793bed87e4621fab7128ae5d73ed17cd81acd7
Tristan Grovender <therealtristangrovender@gmail.com>
2020-05-06 10:42:57 -0500

    MVP Complete

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
+
+// test("renders first name", () => {
+//     const { getByText } = render(<App />);
+//     const linkElement = getByText(/world cup/i);
+//     expect(linkElement).toBeInTheDocument();
+// });
+
+// test("renders last name", () => {
+//     const { getByText } = render(<App />);
+//     const linkElement = getByText(/country/i);
+//     expect(linkElement).toBeInTheDocument();
+// });
+
+// test("renders email", () => {
+//     const { getByText } = render(<App />);
+//     const linkElement = getByText(/searched/i);
+//     expect(linkElement).toBeInTheDocument();
+// });
+
+// test("renders message", () => {
+//     const { getByText } = render(<App />);
+//     const linkElement = getByText(/name/i);
+//     expect(linkElement).toBeInTheDocument();
+// });
